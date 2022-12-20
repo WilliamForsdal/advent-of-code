e00353f34bfbadabeeff8445d6f3d28322b1e150
William Forsdal <william.forsdal@gmail.com>
2022-12-20 01:39:54 +0100

    phew

--- 2022/day17/rust/src/main.rs
+++ 2022/day17/rust/src/main.rs
@@ -15,15 +15,16 @@ impl Shape {
 
     pub fn col(&self, idx: usize) -> [u8; SHAPE_HEIGHT] {
         let s = self.shape;
-        [s[0][0], s[0][1], s[0][2], s[0][3]]
+        [s[0][idx], s[1][idx], s[2][idx], s[3][idx]]
     }
 
     pub fn print(&self) {
         for l in self.shape {
             for c in l {
-                print!("{}")
+                print!("{c}");
             }
-            println!("{l:?}");
+            println!("");
+            // println!("{l:?}");
         }
     }
 }
@@ -55,8 +56,14 @@ fn part1(pushes: &Vec<i32>) {
         // Spawn block
         let mut shape = get_shape(num_block % 5);
         let mut shape_y_offset = highest_point + 3; // spawn block 3 steps above last highest
-
         println!();
+        if num_block > 10 {
+            return;
+        }
+        println!("The cave now looks like this:");
+        for row in &(cave[(highest_point+2)..0]) {
+            println!("{row:?}");
+        }
         loop {
             // push
             shape = push_shape(*(&pushes[push_idx]), &mut shape, &cave, shape_y_offset);
@@ -67,12 +74,13 @@ fn part1(pushes: &Vec<i32>) {
             }
             // if below is rock
             if !can_move(&shape, &cave, shape_y_offset, 0, -1) {
-                for x in 0..SHAPE_WIDTH {
-                    for y in 0..SHAPE_HEIGHT {
-                        let cell = shape.shape[y][x];
-                        if cell == 1 {
-                            cave[y + shape_y_offset][x] = 1;
-                            highest_point = y + shape_y_offset;
+                for (row_idx, row) in shape.shape.iter().enumerate() {
+                    for (col_idx, cell) in row.iter().enumerate() {
+                        if *cell == 1 {
+                            cave[row_idx + shape_y_offset][col_idx] = 1;
+                            if row_idx + shape_y_offset > highest_point {
+                                highest_point = row_idx + shape_y_offset;
+                            }
                         }
                     }
                 }
@@ -81,23 +89,23 @@ fn part1(pushes: &Vec<i32>) {
                 // Move rock one step down and run loop again
                 shape_y_offset -= 1;
             }
-            break;
         }
 
-        for y in (highest_point)..=0 {
-            print!("|");
-            for cell in cave[y] {
-                if cell == 1 {
-                    print!("#");
-                } else {
-                    print!(".");
-                }
-            }
-            println!("|");
-        }
+        // for y in (highest_point)..=0 {
+        //     print!("|");
+        //     for cell in cave[y] {
+        //         if cell == 1 {
+        //             print!("#");
+        //         } else {
+        //             print!(".");
+        //         }
+        //     }
+        //     println!("|");
+        // }
     }
 
-    println!("Part1: not done");
+    println!("Part1: {highest_point}");
+    // 2701 too low
 }
 
 fn push_shape(
@@ -118,10 +126,10 @@ fn push_shape(
             if cx < 0 || cx >= SHAPE_WIDTH as i32 {
                 continue;
             }
-            clone_dst.shape[y][x] = shape.shape[y][cx as usize]
+            clone_dst.shape[y][cx as usize] = shape.shape[y][x];
         }
     }
-    clone_dst.print();
+    // clone_dst.print();
     clone_dst
 }
 
@@ -149,52 +157,50 @@ fn can_move(
                 return false; // Cant move right
             }
         }
+        return true; // we don't check below
     }
 
-    for x in 0..SHAPE_WIDTH {
-        for y in 0..SHAPE_HEIGHT {
-            let cx = x as i32 + xdiff;
-            let cy = y as i32 + ydiff + shape_y_offset as i32;
-            if cx < 0
-                || cx >= SHAPE_WIDTH as i32
-                || x >= SHAPE_WIDTH
-                || cy < 0
-                || cy >= SHAPE_WIDTH as i32
-            {
+    for (row_idx, row) in shape.shape.iter().enumerate() {
+        for (col_idx, cell) in row.iter().enumerate() {
+            let y_below = row_idx as i32 + shape_y_offset as i32 - 1;
+
+            if *cell == 1 && y_below < 0 {
+                return false; // The ground is below!
+            }
+            if y_below < 0 {
+                // We're checking underground but its ok
                 continue;
             }
-            let cx: usize = cx as usize;
-            let cy: usize = cy as usize;
 
-            let cave_cell = cave[cy][cx];
-            let shape_cell = shape.shape[y][x];
-            if shape_cell != 0 && cave_cell != 0 {
-                return false;
+            let cave_cell = cave[y_below as usize][col_idx];
+            if cave_cell == 1 && *cell == 1 {
+                return false; // They would collide.
             }
         }
     }
+
     return true;
 }
 
 fn get_shape(i: usize) -> Shape {
     const SHAPES: [[[u8; 7]; 4]; 5] = [
         [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
         ],
         [
-            [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 1, 0, 0, 0],
             [0, 0, 1, 1, 1, 0, 0],
             [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
         ],
         [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0],
             [0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
         ],
         [
             [0, 0, 1, 0, 0, 0, 0],
@@ -203,10 +209,10 @@ fn get_shape(i: usize) -> Shape {
             [0, 0, 1, 0, 0, 0, 0],
         ],
         [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 1, 1, 0, 0, 0],
             [0, 0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
         ],
     ];
     Shape::new(SHAPES[i])
